43afc778c500fde753be6ae243fab2500bfadbc6
dimitrisad <dsadikin@student.unimelb.edu.au>
2022-10-09 03:11:41 +1100

    Added Form content
    Started section 1, Finished section 4

--- pages/testing.tsx
+++ pages/testing.tsx
@@ -1,4 +1,4 @@
-import { TextInput, Checkbox, Button, Group, Box } from '@mantine/core';
+import { TextInput, Checkbox, Button, Group, Box, Switch, Text, Space, Grid, Stack } from '@mantine/core';
 import { useForm, UseFormReturnType } from '@mantine/form';
 import { AuthAction, withAuthUser, withAuthUserTokenSSR } from 'next-firebase-auth';
 import { Application } from '../lib/interfaces';
@@ -11,15 +11,42 @@ type ApplicationsPageProps = {
 }
 
 type FormDetailsProps = {
-    email: string,
     termsOfService: boolean
+
+    titleProject: string
+
+    principalInvestigator: string
+    jobTitle: string
+    institution: string
+    department: string
+    email: string
+    phone: string
+    addressForm: string
+    city: string
+    stateForm: string
+    zip: string
+    country: string
+
+    reviewer: String
 }
 
 function Demo() {
     const form = useForm({
         initialValues: {
-            email: '',
             termsOfService: false,
+
+            titleProject: '',
+            principalInvestigator: '',
+            jobTitle: '',
+            institution: '',
+            department: '',
+            email: '',
+            phone: '',
+            addressForm: '',
+            city: '',
+            stateForm: '',
+            zip: '',
+            country: '',
         },
 
         validate: {
@@ -28,66 +55,257 @@ function Demo() {
     });
 
     return (
-        <Box sx={{ maxWidth: 300 }} mx="auto">
-          <form onSubmit={form.onSubmit((values) => console.log(values))}>
-            
-            <Section1 form={form} />
-            <Section2 form={form} />
-            <Section3 form={form} />
+        <Box sx={{ maxWidth: 1100 }} mx="auto">
+            <form onSubmit={form.onSubmit((values) => console.log(values))}>
+                <h1>Section Demo</h1>
+                <SectionA form={form} />
+                <SectionB form={form} />
 
-          </form>
+                <h1>Application Details</h1>
+                <Section0 form={form} />
+
+                <h2>Section 1: Investigator and General Information</h2>
+                <Section1 form={form} />
+
+                <h2>Section 2: CCFR Collaborators</h2>
+
+
+                <h2>Section 3: Description of Proposed Collaborative Study</h2>
+                <Section3 form={form} />
+
+                <h2>Section 4: AGREEMENT for us of epidemiologic, pathologic and outcome data and for biomaterials provided from the Colon Cancer Family Registry</h2>
+                <Section4 form={form} />
+
+                <h2>Application Review and Approval</h2>
+
+                <Submit form={form} />
+            </form>
         </Box>
     );
 }
 
 
-function Section1({ form }: { form: UseFormReturnType<FormDetailsProps> }) {
-    //   const form = useForm({
-    //     initialValues: {
-    //       email: '',
-    //       termsOfService: false,
-    //     },
-
-    //     validate: {
-    //       email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
-    //     },
-    //   });
-
+function SectionA({ form }: { form: UseFormReturnType<FormDetailsProps> }) {
     return (
-        
+
         <TextInput
             withAsterisk
             label="Email"
             placeholder="your@email.com"
             {...form.getInputProps('email')}
         />
-                
+
     );
 }
 
-function Section2({ form }: { form: UseFormReturnType<FormDetailsProps> }) {
-    
+function SectionB({ form }: { form: UseFormReturnType<FormDetailsProps> }) {
+
     return (
         <Checkbox
-          mt="md"
-          label="I agree to sell my privacy"
-          {...form.getInputProps('termsOfService', { type: 'checkbox' })}
+            mt="md"
+            label="I agree to sell my privacy"
+            {...form.getInputProps('termsOfService', { type: 'checkbox' })}
         />
 
-                
+
     );
 }
 
-function Section3({ form }: { form: UseFormReturnType<FormDetailsProps> }) {
-    
+function Submit({ form }: { form: UseFormReturnType<FormDetailsProps> }) {
+
     return (
-        
+
         <Group position="right" mt="md">
             <Button type="submit">Submit</Button>
         </Group>
-           
+
     );
 }
+
+function Section0({ form }: { form: UseFormReturnType<FormDetailsProps> }) {
+    return (
+        <TextInput
+            withAsterisk
+            label="Title of Project"
+            {...form.getInputProps('titleProject')}
+        />
+    )
+
+}
+
+function Section1({ form }: { form: UseFormReturnType<FormDetailsProps> }) {
+    return (
+        <>
+            <Group position="center" grow>
+                <TextInput
+                    withAsterisk
+                    label="Principal Investigator"
+                    {...form.getInputProps('principalInvestigator')}
+                />
+                <TextInput
+                    withAsterisk
+                    label="Job Title"
+                    {...form.getInputProps('jobTitle')}
+                />
+            </Group>
+
+            <Group position="center" grow>
+                <TextInput
+                    withAsterisk
+                    label="Institution"
+                    {...form.getInputProps('institution')}
+                />
+                <TextInput
+                    withAsterisk
+                    label="Department"
+                    {...form.getInputProps('department')}
+                />
+            </Group>
+
+            <Group position="center" grow>
+                <TextInput
+                    withAsterisk
+                    label="Email Address"
+                    placeholder='your@email.com'
+                    {...form.getInputProps('email')}
+                />
+                <TextInput
+                    label="Phone Number"
+                    {...form.getInputProps('phone')}
+                />
+            </Group>
+
+            <TextInput
+                withAsterisk
+                label="Address"
+                {...form.getInputProps('addressForm')}
+            />
+
+            <Group position="center" grow>
+                <TextInput
+                    withAsterisk
+                    label="City/Suburb"
+                    {...form.getInputProps('city')}
+                />
+                <TextInput
+                    withAsterisk
+                    label="State"
+                    {...form.getInputProps('stateForm')}
+                />
+            </Group>
+
+            <Group position="center" grow>
+                <TextInput
+                    withAsterisk
+                    label="Zip/Post Code"
+                    {...form.getInputProps('zip')}
+                />
+                <TextInput
+                    withAsterisk
+                    label="Country"
+                    {...form.getInputProps('country')}
+                />
+            </Group>
+            <Space h="lg" />
+
+            <Grid>
+                <Grid.Col span={8}>Will a 2nd institution require access to CCFR data/biospecimens be involved in your study?</Grid.Col>
+                <Grid.Col span={2}><Switch onLabel="Yes" offLabel="No" size="xl" /></Grid.Col>
+            </Grid>
+            <Space h="lg" />
+
+            <Grid>
+                <Grid.Col span={8}>If requesting CCFR biospecimens, will they be used for product commercialization?</Grid.Col>
+                <Grid.Col span={2}><Switch onLabel="Yes" offLabel="No" size="xl" /></Grid.Col>
+            </Grid>
+
+        </>
+
+    )
+
+}
+
+function Section2({ form }: { form: UseFormReturnType<FormDetailsProps> }) {
+    return (
+        <>
+        </>
+    )
+}
+
+function Section3({ form }: { form: UseFormReturnType<FormDetailsProps> }) {
+    return (
+        <>
+            <Text>Please upload a brief description of the proposed research to use data/biospecimens from the CCFR.</Text>
+        </>
+    )
+}
+
+function Section4({ form }: { form: UseFormReturnType<FormDetailsProps> }) {
+    return (
+        <>
+            <Stack align="flex-start">
+                <Checkbox
+                    label="I agree to form a collaboration with the Colon CFR (CCFR). I agree to assume all risks and responsibilities in connection with the receipt, handling, storage and
+                    use of data/biomaterials. I agree that the data/biomaterials to be provided by the NCI CCFR will be used only for the purposes specified in the approved proposal.
+                    I will provide documentation of IRB/ethics committee review that will include my IRB file number and IRB review date. I agree not to distribute data or biomaterials
+                    to third parties without the approval of the CCFR Principal Investigators and then only with fully executed data-use agreement and/or material transfer agreement."
+                />
+                <Checkbox
+                    label="I agree to make study-generated results available to the scientific community by transferring them to the central CCFR Informatics Center within 6 months of their
+                    publication and to submit progress reports upon request (at most annually) until the project is completed."
+                />
+                <Checkbox
+                    label=" agree to adhere to the CCFR Policy on Publications and notify the CCFR of planned publications that make use of CCFR data and/or biospecimens and to:
+                    1) Register publications with the CCFR early in the planning process;
+                    2) Submit publications to the CFR for administrative review prior to submission to a journal; and,
+                    3) Acknowledge the contributions (financial and otherwise) of the NCI and CCFR. The CCFR Policy on Publications, Manuscript Registration Form and Review
+                        Checklist can be downloaded at www.coloncfr.org/publications."
+                />
+                <Checkbox
+                    label="I understand that the Colon CFR has been funded entirely by the NCI of the U.S. NIH, and that all applicable publications arising from the use of Colon CFR resources
+                    must comply with the NIH Public Access Policy by ensuring they are submitted to the PubMed Central (PMC) upon acceptance for publication
+                    (see: https://www.nlm.nih.gov/bsd/public_access/resources.html)."
+                />
+                <Space h="xl" />
+                <Text size="sm">
+                    <Text weight={700}>[Recommended funding acknowledgement]</Text> "Research reported in this publication was supported in part by the National Cancer Institute (NCI) of the National Institutes
+                    of Health (NIH) under award number U01 CA167551. The content of this manuscript does not necessarily reflect the views or policies of the NIH or any of the collaborating
+                    centers in the Colon Cancer Family Registry (CCFR), nor does mention of trade names, commercial products, or organizations imply endorsement by the US Government
+                    or the CCFR.” [Additional funding acknowledgement for the manuscripts utilizing CCFR GWAS data can be found in our Policy for Publications www.coloncfr.org/publications.]
+                </Text>
+                <Text size="sm">
+                    This document formalizes the agreement between the applicant and site(s) to collaborate.
+                </Text>
+            </Stack>
+            <Space h="xl" />
+
+        </>
+    )
+}
+
+function Section5({ form }: { form: UseFormReturnType<FormDetailsProps> }) {
+    return (
+        <>
+            <Text>
+                The above application has been reviewed and approved by:
+            </Text>
+            <Group>
+                <TextInput
+                    withAsterisk
+                    label="Email Address"
+                    placeholder='your@email.com'
+                    {...form.getInputProps('email')}
+                />
+            </Group>
+
+        </>
+    )
+}
+
+
+
+
+// below is the code required to ensure user is authenticated 
 
 export const getServerSideProps = withAuthUserTokenSSR({
     whenUnauthed: AuthAction.REDIRECT_TO_LOGIN,
