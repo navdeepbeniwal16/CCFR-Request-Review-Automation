899bb912f9ff73e73e35e44bfd1d6435adc4b5c5
denin <navdeep.beniwal1997@gmail.com>
2022-10-09 22:40:10 +1100

    refactor: added applications and user module files

--- lib/application.ts
+++ lib/application.ts
@@ -1,262 +1,326 @@
-import { Application } from "./interfaces"
-import firebase from "firebase";
-import { ApplicationStage, ApplicationStatus, DBCollections } from "./utilities/AppEnums";
+import { Application } from './interfaces';
+import firebase from 'firebase';
+import {
+    ApplicationStage,
+    ApplicationStatus,
+    DBCollections,
+} from './utilities/AppEnums';
+import { printErrorTrace } from './utilities/errorHandler';
 
 const initEmptyApplication = () => {
-    let application:Application = {} as Application;
+    let application: Application = {} as Application;
     application.isEmpty = (): Boolean => {
         return Object.keys(application).length === 1;
-    }
+    };
 
     return application;
-}
+};
 
-const createApplication = (data:firebase.firestore.DocumentData | Application | undefined) => {
-    const application:Application = <Application> data;
+const createApplication = (
+    data: firebase.firestore.DocumentData | Application | undefined,
+) => {
+    const application: Application = <Application>data;
     application.isEmpty = (): Boolean => {
         return Object.keys(application).length === 1;
-    }
+    };
 
     return application;
-}
+};
 
 const createApplicationData = (application: Application) => {
-    const data:any = {};
+    const data: any = {};
     for (const [k, v] of Object.entries(application)) {
-        if(k == 'isEmpty') continue;
+        if (k == 'isEmpty') continue;
         data[k] = v;
-      }
+    }
     return data;
-}
+};
 
-export const saveApplicationAsDraft = async (application: Application, auth: firebase.auth.Auth, db: firebase.firestore.Firestore) => {
-    application.stage = ApplicationStage.Draft;  
-    await db.collection(DBCollections.Applications).doc(application.id).set(application)
-    .then(() => {
-        console.log("Application document successfully written!");
-    })
-    .catch((error) => {
-        console.log("Error writing application document: ", error.message);
-        return false;
-    });
+export const saveApplicationAsDraft = async (application: Application) => {
+    application.stage = ApplicationStage.Draft;
+
+    await firebase
+        .firestore()
+        .collection(DBCollections.Applications)
+        .doc(application.id)
+        .set(application)
+        .then(() => {
+            console.log('Application document successfully written!');
+        })
+        .catch(error => {
+            printErrorTrace(saveApplicationAsDraft, error, false);
+            return false;
+        });
 
     return true;
-}
+};
 
-export const saveAndSubmitApplication = async (application: Application, auth: firebase.auth.Auth, db: firebase.firestore.Firestore) => {
+export const saveAndSubmitApplication = async (application: Application) => {
     application.stage = ApplicationStage.Submitted;
     application.status = ApplicationStatus.Active;
-    await db.collection(DBCollections.Applications).doc(application.id).set(application)
-    .then(() => {
-        console.log("Application document successfully written!");
-    })
-    .catch((error) => {
-        console.log("Error writing application document: ", error.message);
-        return false;
-    });
+    await firebase
+        .firestore()
+        .collection(DBCollections.Applications)
+        .doc(application.id)
+        .set(application)
+        .then(() => {
+            console.log('Application document successfully written!');
+        })
+        .catch(error => {
+            printErrorTrace(saveAndSubmitApplication, error, false);
+            return false;
+        });
 
     return true;
-}
+};
 
-export const getApplicationById = async (applicationId: string, auth: firebase.auth.Auth, db: firebase.firestore.Firestore) => {
-  const docRef = db.collection(DBCollections.Applications).doc(applicationId);
-  let fetchedApplication:Application = initEmptyApplication();
-  await docRef.get().then((doc) => {
-      if (doc.exists) {
-        fetchedApplication = createApplication(doc.data())
-          console.log("Application data with id (" + applicationId + ":", fetchedApplication); // TODO: TBR after testing..
-          return fetchedApplication;
-      } else {
-        console.log("No application found");
-      }
-  }).catch((error) => {
-      console.log("Error getting document:", error.message);
-  });
+export const getApplicationById = async (applicationId: string) => {
+    const docRef = firebase
+        .firestore()
+        .collection(DBCollections.Applications)
+        .doc(applicationId);
+    let fetchedApplication: Application = initEmptyApplication();
+    await docRef
+        .get()
+        .then(doc => {
+            if (doc.exists) {
+                console.log(doc.id, ' => ', doc.data());
+                fetchedApplication = createApplication(doc.data());
+                return fetchedApplication;
+            } else {
+                console.log('No application found');
+            }
+        })
+        .catch(error => {
+            printErrorTrace(getApplicationById, error, false);
+        });
 
-  return fetchedApplication;
-}
+    return fetchedApplication;
+};
 
-export const getApplicationByTitle = async (applicationTitle: string, auth: firebase.auth.Auth, db: firebase.firestore.Firestore) => {
-  let fetchedApplications:Application[] = [];
-  
-  const docRef = db.collection(DBCollections.Applications).where("title", "==", applicationTitle);
-  await docRef.get()
-  .then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-          console.log(doc.id, " => ", doc.data());
-          const application = createApplication(doc.data());
-          fetchedApplications.push(application);
-      });
-  })
-  .catch((error) => {
-      console.log("Error getting documents: ", error.message);
-  });
-  
-  if(fetchedApplications.length > 0) {
-    return fetchedApplications[0];
-  } else {
-    return initEmptyApplication();
-  }
-}
+export const getApplicationByTitle = async (applicationTitle: string) => {
+    let fetchedApplications: Application[] = [];
 
-export const getAllSubmittedApplications = async (auth: firebase.auth.Auth, db: firebase.firestore.Firestore) => {
-  let fetchedApplications:Application[] = [];
+    const docRef = firebase
+        .firestore()
+        .collection(DBCollections.Applications)
+        .where('title', '==', applicationTitle);
+    await docRef
+        .get()
+        .then(querySnapshot => {
+            querySnapshot.forEach(doc => {
+                console.log(doc.id, ' => ', doc.data());
+                const application = createApplication(doc.data());
+                fetchedApplications.push(application);
+            });
+        })
+        .catch(error => {
+            printErrorTrace(getApplicationByTitle, error, false);
+        });
 
-  const docRef = db.collection(DBCollections.Applications).where("stage", "==", ApplicationStage.Submitted);
-  await docRef.get()
-  .then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-          console.log(doc.id, " => ", doc.data());
-          const application = createApplication(doc.data());
-          fetchedApplications.push(application);
-      });
-  })
-  .catch((error) => {
-      console.log("Error getting documents: ", error.message);
-  });
+    if (fetchedApplications.length > 0) {
+        return fetchedApplications[0];
+    } else {
+        return initEmptyApplication();
+    }
+};
 
-  return fetchedApplications;
-}
+export const getAllSubmittedApplications = async () => {
+    let fetchedApplications: Application[] = [];
 
-export const getSavedApplicationsByApplicant = async (applicantEmail: string, auth: firebase.auth.Auth, db: firebase.firestore.Firestore) => {
-  let fetchedApplications:Application[] = [];
+    const docRef = firebase
+        .firestore()
+        .collection(DBCollections.Applications)
+        .where('stage', '==', ApplicationStage.Submitted);
+    await docRef
+        .get()
+        .then(querySnapshot => {
+            querySnapshot.forEach(doc => {
+                console.log(doc.id, ' => ', doc.data());
+                const application = createApplication(doc.data());
+                fetchedApplications.push(application);
+            });
+        })
+        .catch(error => {
+            printErrorTrace(getAllSubmittedApplications, error, false);
+        });
 
-  const docRef = db.collection(DBCollections.Applications).where("email", "==", applicantEmail).where("stage", "==", ApplicationStage.Draft);
-  await docRef.get()
-  .then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-          // doc.data() is never undefined for query doc snapshots
-          console.log(doc.id, " => ", doc.data());
-          const application = createApplication(doc.data());
-          fetchedApplications.push(application);
-      });
-  })
-  .catch((error) => {
-      console.log("Error getting documents: ", error.message);
-  });
+    return fetchedApplications;
+};
 
-  return fetchedApplications;
-}
+export const getSavedApplicationsByApplicant = async (
+    applicantEmail: string,
+) => {
+    let fetchedApplications: Application[] = [];
 
-export const getSubmittedApplicationsByApplicant = async (applicantEmail: string, auth: firebase.auth.Auth, db: firebase.firestore.Firestore) => {
-  let fetchedApplications:Application[] = [];
+    const docRef = firebase
+        .firestore()
+        .collection(DBCollections.Applications)
+        .where('email', '==', applicantEmail)
+        .where('stage', '==', ApplicationStage.Draft);
+    await docRef
+        .get()
+        .then(querySnapshot => {
+            querySnapshot.forEach(doc => {
+                console.log(doc.id, ' => ', doc.data());
+                const application = createApplication(doc.data());
+                fetchedApplications.push(application);
+            });
+        })
+        .catch(error => {
+            printErrorTrace(getSavedApplicationsByApplicant, error, false);
+        });
 
-const docRef = db.collection(DBCollections.Applications).where("email", "==", applicantEmail).where("stage", "==", ApplicationStage.Submitted);
-  await docRef.get()
-  .then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-          console.log(doc.id, " => ", doc.data());
-          const application = createApplication(doc.data());
-          fetchedApplications.push(application);
-      });
-  })
-  .catch((error) => {
-      console.log("Error getting documents: ", error.message);
-  });
+    return fetchedApplications;
+};
 
-  return fetchedApplications;
-}
+export const getSubmittedApplicationsByApplicant = async (
+    applicantEmail: string,
+) => {
+    let fetchedApplications: Application[] = [];
 
-export const getApplicationsByStatus = async (status: ApplicationStatus, auth: firebase.auth.Auth, db: firebase.firestore.Firestore) => {
-  let fetchedApplications:Application[] = [];
+    const docRef = firebase
+        .firestore()
+        .collection(DBCollections.Applications)
+        .where('email', '==', applicantEmail)
+        .where('stage', '==', ApplicationStage.Submitted);
+    await docRef
+        .get()
+        .then(querySnapshot => {
+            querySnapshot.forEach(doc => {
+                console.log(doc.id, ' => ', doc.data());
+                const application = createApplication(doc.data());
+                fetchedApplications.push(application);
+            });
+        })
+        .catch(error => {
+            printErrorTrace(getSubmittedApplicationsByApplicant, error, false);
+        });
 
-  const docRef = db.collection(DBCollections.Applications).where("status", "==", status);
-  await docRef.get()
-  .then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-          console.log(doc.id, " => ", doc.data());
-          const application = createApplication(doc.data());
-          fetchedApplications.push(application);
-      });
-  })
-  .catch((error) => {
-      console.log("Error getting documents: ", error.message);
-  });
+    return fetchedApplications;
+};
 
-  return fetchedApplications;
-}
+export const getApplicationsByStatus = async (status: ApplicationStatus) => {
+    let fetchedApplications: Application[] = [];
 
-export const getApplicationsByStage = async (stage: ApplicationStage, auth: firebase.auth.Auth, db: firebase.firestore.Firestore) => {
-  let fetchedApplications:Application[] = [];
+    const docRef = firebase
+        .firestore()
+        .collection(DBCollections.Applications)
+        .where('status', '==', status);
+    await docRef
+        .get()
+        .then(querySnapshot => {
+            querySnapshot.forEach(doc => {
+                console.log(doc.id, ' => ', doc.data());
+                const application = createApplication(doc.data());
+                fetchedApplications.push(application);
+            });
+        })
+        .catch(error => {
+            printErrorTrace(getApplicationsByStatus, error, false);
+        });
 
-  const docRef = db.collection(DBCollections.Applications).where("stage", "==", stage);
-  await docRef.get()
-  .then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-          console.log(doc.id, " => ", doc.data());
-          const application = createApplication(doc.data());
-          fetchedApplications.push(application);
-      });
-  })
-  .catch((error) => {
-      console.log("Error getting documents: ", error.message);
-  });
+    return fetchedApplications;
+};
 
-  return fetchedApplications;
-}
+export const getApplicationsByStage = async (stage: ApplicationStage) => {
+    let fetchedApplications: Application[] = [];
+
+    const docRef = firebase
+        .firestore()
+        .collection(DBCollections.Applications)
+        .where('stage', '==', stage);
+    await docRef
+        .get()
+        .then(querySnapshot => {
+            querySnapshot.forEach(doc => {
+                console.log(doc.id, ' => ', doc.data());
+                const application = createApplication(doc.data());
+                fetchedApplications.push(application);
+            });
+        })
+        .catch(error => {
+            printErrorTrace(getApplicationsByStage, error, false);
+        });
+
+    return fetchedApplications;
+};
 
 // TODO:
-export const withdrawApplication = async (applicationId: string, auth: firebase.auth.Auth, db: firebase.firestore.Firestore) => {
-  let application:Application = await getApplicationById(applicationId, auth, db);
-  let isWithdrawn = false;
-  if(!application.isEmpty()) {
-    application.stage = ApplicationStage.Draft;  // TODO: Check with Saood, it probably should be apart of the status rather than Stage
-    const data = createApplicationData(application);
-    await db.collection(DBCollections.Applications).doc(application.id).set(data)
-    .then(() => {
-        console.log("Application withdrawn successfully!");
-        isWithdrawn = true;
-    })
-    .catch((error) => {
-        console.error("Error writing withdrawing application: ", error.message);
-        isWithdrawn = false;
-    });
-  }
-  
-  return isWithdrawn;
-}
-
-export const updateApplicationStatus = async (applicationId: string, status:ApplicationStatus, auth: firebase.auth.Auth, db: firebase.firestore.Firestore) => {
-  let application = await getApplicationById(applicationId, auth, db);
-  let isStatusChanged = false;
-  if(!application.isEmpty()) {
-    application.status = status;
-    const applicationData = createApplicationData(application);
-    await db.collection(DBCollections.Applications).doc(application.id).set(applicationData)
-    .then(() => {
-        console.log("Application withdrawn successfully!");
-        isStatusChanged = true;
-    })
-    .catch((error) => {
-        console.error("Error writing withdrawing application: ", error.message);
-        isStatusChanged = false;
-    });
-  }
-  
-  return isStatusChanged;
-}
-
-export const updateApplication = async (application: Application, auth: firebase.auth.Auth, db: firebase.firestore.Firestore) => {
-  let isStatusChanged = false;
-  application = createApplication(application);
-  if(application.isEmpty() && application.id) {
-    let foundApplication = await getApplicationById(application.id, auth, db);
-  
-    if(!foundApplication.isEmpty()) {
-      const applicationData = createApplicationData(application);
-      await db.collection(DBCollections.Applications).doc(application.id).set(applicationData)
-      .then(() => {
-          console.log("Application updated successfully!");
-          isStatusChanged = true;
-      })
-      .catch((error) => {
-          console.error("Error updating applications: ", error.message);
-          isStatusChanged = false;
-      });
+export const withdrawApplication = async (applicationId: string) => {
+    let application: Application = await getApplicationById(applicationId);
+    let isWithdrawn = false;
+    if (!application.isEmpty()) {
+        application.stage = ApplicationStage.Draft; // TODO: Check with Saood, it probably should be apart of the status rather than Stage
+        const data = createApplicationData(application);
+        await firebase
+            .firestore()
+            .collection(DBCollections.Applications)
+            .doc(application.id)
+            .set(data)
+            .then(() => {
+                console.log('Application withdrawn successfully!');
+                isWithdrawn = true;
+            })
+            .catch(error => {
+                printErrorTrace(withdrawApplication, error, false);
+                isWithdrawn = false;
+            });
     }
-  }
-  
-  return isStatusChanged;
-}
 
-  
+    return isWithdrawn;
+};
+
+export const updateApplicationStatus = async (
+    applicationId: string,
+    status: ApplicationStatus,
+) => {
+    let application = await getApplicationById(applicationId);
+    let isStatusChanged = false;
+    if (!application.isEmpty()) {
+        application.status = status;
+        const applicationData = createApplicationData(application);
+        await firebase
+            .firestore()
+            .collection(DBCollections.Applications)
+            .doc(application.id)
+            .set(applicationData)
+            .then(() => {
+                console.log('Application withdrawn successfully!');
+                isStatusChanged = true;
+            })
+            .catch(error => {
+                printErrorTrace(updateApplicationStatus, error, false);
+                isStatusChanged = false;
+            });
+    }
+
+    return isStatusChanged;
+};
+
+export const updateApplication = async (application: Application) => {
+    let isStatusChanged = false;
+    application = createApplication(application);
+    if (application.isEmpty() && application.id) {
+        let foundApplication = await getApplicationById(application.id);
+
+        if (!foundApplication.isEmpty()) {
+            const applicationData = createApplicationData(application);
+            await firebase
+                .firestore()
+                .collection(DBCollections.Applications)
+                .doc(application.id)
+                .set(applicationData)
+                .then(() => {
+                    console.log('Application updated successfully!');
+                    isStatusChanged = true;
+                })
+                .catch(error => {
+                    printErrorTrace(updateApplication, error, false);
+                    isStatusChanged = false;
+                });
+        }
+    }
+
+    return isStatusChanged;
+};
--- lib/user.ts
+++ lib/user.ts
@@ -1,68 +1,70 @@
-import firebase from 'firebase/app'
+import firebase from 'firebase/app';
 import { UserProfile } from './interfaces';
+import { printErrorTrace } from './utilities/errorHandler';
 
 export const registerUser = async (email: string, password: string) => {
-    return firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-        // Signed in 
-        return userCredential.user;
-    })
-    .catch((error) => {
-        printErrorTrace(registerUser, error, false)
-    });
-}
+    return firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(userCredential => {
+            // Signed in
+            return userCredential.user;
+        })
+        .catch(error => {
+            printErrorTrace(registerUser, error, false);
+        });
+};
 
 export const getCurrentUser = () => {
     return firebase.auth().currentUser;
-}
+};
 
 export const loginUser = async (email: string, password: string) => {
-    return firebase.auth().signInWithEmailAndPassword(email, password)
-        .catch((error) => {
+    return firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .catch(error => {
             printErrorTrace(loginUser, error, false);
         });
-} 
+};
+export const logoutUser = (callback: { (): Promise<boolean>; (): any }) => {
+    firebase
+        .auth()
+        .signOut()
+        .then(() => callback());
+};
 
-export const logoutUser = (callback: { (): Promise<boolean>; (): any; }) => {
-    firebase.auth().signOut().then(() => callback())
-}
-
-export const updateUserProfile = async (userProfile:UserProfile) => {
+export const updateUserProfile = async (userProfile: UserProfile) => {
     const user = firebase.auth().currentUser;
     let isUpdated = false;
 
-    if(user) {
-        await user.updateProfile(userProfile).then(() => {
-            isUpdated = true;
-        }).catch((error) => {
-            printErrorTrace(updateUserProfile, error, false);
-        });
+    if (user) {
+        await user
+            .updateProfile(userProfile)
+            .then(() => {
+                isUpdated = true;
+            })
+            .catch(error => {
+                printErrorTrace(updateUserProfile, error, false);
+            });
     }
 
     return isUpdated;
-}
+};
 
 export const deleteUser = async () => {
     const user = firebase.auth().currentUser;
     let isDeleted = false;
-    if(user) {
-        await user.delete().then(() => {
-            isDeleted = true;
-        }).catch((error) => {
-            printErrorTrace(deleteUser, error, false);
-        });
+    if (user) {
+        await user
+            .delete()
+            .then(() => {
+                isDeleted = true;
+            })
+            .catch(error => {
+                printErrorTrace(deleteUser, error, false);
+            });
     }
 
     return isDeleted;
-}
-
-const printErrorTrace = (func: any, error: any, includeStackTrace: boolean) => {
-    console.log(`Error occured at ${func.name}`);
-    console.log('Error code : ' + error.errorCode);
-    console.log('Error message : ' + error.message);
-    
-    if(includeStackTrace) {
-        console.log('Stack trace is ON');
-        console.log(error);
-    }
-}
+};
